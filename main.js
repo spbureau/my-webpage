f94ec2124ff5590203055935912568add4f22cd2
Simon Peters <134172865+spbureau@users.noreply.github.com>
2023-08-27 13:51:49 +0000

    Update main.js with some functions

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
+// my scripts 
+
+// my objects
+
+const person1 = {
+  name: "Simon Peters",
+  lastName: "SAKPLA",
+  fullName: function(){
+    return this.name + " " + this.lastName;
+  },
+  age: "23",
+  sex: "M",
+  school: "KNUST"
+};
+
+const person2 = {
+  name: "Jenniferrrrrrrrrr",
+  age: "21",
+  sex: "F",
+  school: "KNUST (BIT)"
+};
+
+
 console.log('Hello World!');
+let x = addNumbers(5, 6);
+let y = addNumbers(21, 6);
+
+console.log(x + y);
+
+console.log(person2.name);
+console.log(`Length is ${person2.name.length}`);
+
+console.log(`His name is ${person1.name}, ${person1.age}, full name is ${person1.fullName()};
+and she is ${person2.name}, ${person2.age}.`)
 
 /*
 function insetInPage{
@@ -6,4 +39,24 @@ function insetInPage{
 }
 */
 
+// html manipulation 
 document.getElementById("greeting-js").innerHTML='Hi world, JavaScript here';
+
+// show time now
+function showTime(){
+  console.log(Date());
+  document.getElementById('time').innerHTML=Date();
+};
+
+function showTimeOnLoad(){
+  console.log(Date());
+  document.getElementById('default-time').innerHTML=Date();
+};
+
+// my functions
+function addNumbers(num1, num2){
+  let ans = num1 ^ num2;
+  console.log("Answer is: " + ans);
+  return ans;
+};
+
