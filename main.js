fbe43a4b22a4b4981ca2c1472a67326803c44b7d
simonpetersonline@gmail.com <simonpetersonline@gmail.com>
2023-08-28 15:55:31 +0000

    edited

--- main.js
+++ main.js
@@ -31,7 +31,15 @@ console.log(person2.name);
 console.log(`Length is ${person2.name.length}`);
 
 console.log(`His name is ${person1.name}, ${person1.age}, full name is ${person1.fullName()};
-and she is ${person2.name}, ${person2.age}.`)
+and she is ${person2.name}, ${person2.age}.`);
+
+let text = "Please visit Microsoft!";
+let newText = text.replace("Microsoft", "W3Schools");
+
+console.log(text);
+console.log(newText);
+console.log(newText.replace("W3Schools", "SP Bureau"));
+
 
 /*
 function insetInPage{
@@ -42,12 +50,44 @@ function insetInPage{
 // html manipulation 
 document.getElementById("greeting-js").innerHTML='Hi world, JavaScript here';
 
+
 // show time now
 function showTime(){
   console.log(Date());
   document.getElementById('time').innerHTML=Date();
 };
 
+/*
+// show arrays
+const mypeople = ["Simon Peters", "Jenniferrrrrrrrrr", "Blay", "Sweet Melanin", "Delight"];
+let peopleLength = mypeople.length;
+
+let text = "<ul>";
+
+for (let p = 0; p < peopleLength; p++) {
+  text += "<li>" + mypeople[p] + "</li>";
+}
+
+text += "</ul>";
+
+document.getElementById("array").innerHTML=text;
+
+*/
+
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
+
+document.getElementById("demo").innerHTML = text;
+*/
+
+// show time on load
 function showTimeOnLoad(){
   console.log(Date());
   document.getElementById('default-time').innerHTML=Date();
@@ -60,3 +100,23 @@ function addNumbers(num1, num2){
   return ans;
 };
 
+
+// time of day greeting 
+//function greetings(){
+  
+//  let dateNow = new Date(2023, 7, 28, 9, 45);
+  let dateNow = new Date(2018, 11, 24, 19, 33);
+  let hourNow = dateNow.getHours;
+  
+  let greeting;
+  if (hourNow < 12){
+    greeting = "Good morning!";
+  }
+  else if(hourNow => 12 && hourNow < 17){
+    greeting = "Good afternoon!";
+  }
+  else{
+    greeting = "Good evening!"
+  }
+  document.getElementById("greetings").innerHTML=greeting;
+//}
