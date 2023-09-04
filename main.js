9f9cfe19be4941040ea7c9569f1d29e0ea1c9406
simonpetersonline@gmail.com <simonpetersonline@gmail.com>
2023-09-04 18:36:02 +0000

    ...not working

--- main.js
+++ main.js
@@ -139,6 +139,37 @@ let dateNow = new Date();
 let list = "";
 for(let i =0; i < 5; i++){
   list += "List " + i + "</br>";
-}
+};
 
 document.getElementById('list').innerHTML=list;
+
+
+// // // // // 
+ // countdown timer
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var timeNow = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - timeNow;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById('countdown').innerHTML = days + "d : " + hours + "h : " + minutes + "m : " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('countdown').innerHTML = "EXPIRED";
+  }
+}, 1000);
