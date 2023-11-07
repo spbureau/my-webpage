cdf287590174a1cec9a00dc00bb9a52a8fcb95c3
simonpetersonline@gmail.com <simonpetersonline@gmail.com>
2023-11-07 21:13:26 +0000

    last commit, 7/11/23

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ document.getElementById('list').innerHTML=list;
 // // // // // 
  // countdown timer
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+var countDownDate = new Date("2024-01-01T00:00:01Z").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
